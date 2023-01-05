9196f169f73ceacf936a8a2decb4f1d5241d572d
Lorenzo <86910177+ignotochi@users.noreply.github.com>
2023-01-05 14:47:18 +0100

    WIP (#3)

--- src/app/components/home/meross-home.component.ts
+++ src/app/components/home/meross-home.component.ts
@@ -1,6 +1,7 @@
 import {
-    AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewContainerRef
+    AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewChild, ViewContainerRef
 } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 import { BadgeStatus } from 'src/app/core/components/badge-status/badge-status.component';
 import { Auth } from 'src/app/services/auth.service';
 import { LoadMerossDevices } from '../load-devices/load-devices.component';
@@ -15,6 +16,9 @@ import { LoadMerossDevices } from '../load-devices/load-devices.component';
 })
 
 export class MerossHome implements OnInit, AfterViewInit, OnDestroy {
+
+    public options = { language: 'it-IT' , polling: false, columns: 4 };
+    
     constructor(private containerRef: ViewContainerRef, public auth: Auth) {
     }
 
@@ -27,5 +31,38 @@ export class MerossHome implements OnInit, AfterViewInit, OnDestroy {
     }
 
     ngOnDestroy(): void {
+    }
+
+    changeLanguage(value: string): string {   
+        if (value === 'it-IT') {
+            this.options.language = 'en-EN';
+            return 'en-EN';
+        }
+        else {
+            this.options.language = 'it-IT';
+            return 'it-IT';
+        }
+    }
+
+    changeGridColumns(value: number): string {
+        if (value === 4) {
+            this.options.columns = 8;
+            return '8';
+        }
+        else {
+            this.options.columns = 4;
+            return '4';
+        }
+    }
+
+    enablePolling(value: boolean): string {
+        if (value === true) {
+            this.options.polling = false;
+            return 'Disattivo';
+        }
+        else {
+            this.options.polling = true;
+            return 'Attivo';
+        }
     }
 }
