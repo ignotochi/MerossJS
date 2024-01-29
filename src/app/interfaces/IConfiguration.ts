7befd2cdd143601006f90075f43f6ea67bfedc5d
ignotochi <ignotochi@gmail.com>
2024-01-29 22:13:08 +0100

    WIP

--- src/app/interfaces/IConfiguration.ts
+++ src/app/interfaces/IConfiguration.ts
@@ -2,7 +2,7 @@ import { language } from "../enum/enums";
 
 export interface IConfiguration {
     language: language,
-    baseUrl : string;
+    marossApiUrl : string;
     port: string;
     protocol: string,
     version: string
